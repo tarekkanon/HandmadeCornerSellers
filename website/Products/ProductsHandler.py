1a0d797b441cc44f54ba50170191ed6b22a5ebe4
Tarek Kanon <tarek.kanon@gmail.com>
2023-07-06 22:07:42 +0300

    edited html

--- website/Products/ProductsHandler.py
+++ website/Products/ProductsHandler.py
@@ -8,7 +8,16 @@ products = Blueprint('products', __name__)
 @products.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    if request.method == 'POST': 
-            flash('Note added!', category='success')
 
-    return render_template("seller_panel.html", user=current_user)
+        page = {}
+        page['home'] = False
+        page['products'] = True
+        page['orders'] = False
+
+        content = {'page' : page , 'user' : current_user, 'inside_content_title' : 'Title test'}
+
+
+        if request.method == 'POST': 
+                flash('Note added!', category='success')
+
+        return render_template("seller_panel.html" , model = content)
